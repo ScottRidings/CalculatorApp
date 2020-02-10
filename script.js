d210fad5420cda03ff4742c2436e1bd25a82077b
swridings <27883567swridings@user.noreply.github.com>
2020-02-10 16:56:21 -0500

    Created calculator layout

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // ---------------------- Page Load ---------------------- //
+window.onload = function() {
+    let myTable = document.getElementById('table-buttons');
+    let tableBody = myTable.getElementsByTagName('tbody')[0];
+    let tableWidth = tableBody.offsetWidth;
+    document.getElementById('result').style.width = tableWidth - 4 + 'px';
+}

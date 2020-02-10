8eb2914c8c5ee3a82111d846e0ed883ee6bf7162
swridings <27883567swridings@user.noreply.github.com>
2020-02-10 18:12:42 -0500

    Added ability to enter numbers via mouse or keyboard

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ window.onload = function() {
     let tableWidth = tableBody.offsetWidth;
     document.getElementById('result').style.width = tableWidth - 4 + 'px';
 }
+
+var screenTextIsNothing = true;
+var decimalInUse = false;
+var opperatorInUse = false;
+var screenText;
+
+document.body.addEventListener('click', function (evt) {
+    editScreenText(evt.target.innerText);
+}, false);
+
+document.body.addEventListener('keypress', function (evt) {
+    editScreenText(evt.key);
+}, false);
+
+function editScreenText(value) {
+    if (! isNaN(value) || (value == '.' && decimalInUse === false)) {
+        if (screenTextIsNothing === true) {
+            screenText = value;
+            screenTextIsNothing = false;
+        } else {
+            screenText = screenText + value;
+        }
+
+        if (value == '.') decimalInUse = true;
+        document.getElementById('result').innerText = screenText;
+    }
+}

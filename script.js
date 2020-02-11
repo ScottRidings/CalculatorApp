dc34e5ac20f8e75fbd6a5db34e097eb1015a1cd5
swridings <27883567swridings@user.noreply.github.com>
2020-02-11 02:20:59 -0500

    Added the use math operations to the operator buttons and keys

--- script.js
+++ script.js
@@ -3,89 +3,173 @@ window.onload = function() {
     let myTable = document.getElementById('table-buttons');
     let tableBody = myTable.getElementsByTagName('tbody')[0];
     let tableWidth = tableBody.offsetWidth;
-    document.getElementById('result').style.width = tableWidth - 4 + 'px';
+    document.getElementById('display-tape').style.width = tableWidth - 4 + 'px';
+    document.getElementById('display').style.width = tableWidth - 4 + 'px';
 }
 
-var screenTextIsNothing = true;
+var displayText;
+var displayTextIsNothing = true;
+var displayTapeText = '';
 var decimalInUse = false;
-var opperatorInUse = false;
-var screenText;
-var firstValue = 0;
+var operatorInUse = '';
+var valueLeftOfOperator = 0;
 
-// -------------------- Button Click -------------------- //
+
+// ---------------- Use Operator Buttons ---------------- //
+function useOperator(value) {
+    let displayValue = parseFloat(document.getElementById('display').innerText);
+
+    if (displayTapeText == '' || operatorInUse == '=') {
+        valueLeftOfOperator = displayValue;
+        displayTapeText = displayValue.toString() + ' ' + value + ' ';
+    } else if(displayTextIsNothing == true && operatorInUse != '') {
+        displayTapeText = displayTapeText.substring(0, displayTapeText.length - 2) + value + ' ';
+    } else {
+        valueLeftOfOperator = preformMathOperation(displayValue);
+        displayTapeText = displayTapeText + displayText + ' ' + value + ' ';
+        displayText = valueLeftOfOperator.toString();
+        document.getElementById('display').innerText = displayText;
+    }
+
+    operatorInUse = value;
+    displayTextIsNothing = true;
+    decimalInUse = false;
+    document.getElementById('display-tape').innerText = displayTapeText;
+}
+
+function preformMathOperation(displayValue) {
+    let newValue = 0;
+    if (operatorInUse == '÷') {
+        newValue = valueLeftOfOperator / displayValue;
+    } else if (operatorInUse == '×') {
+        newValue = valueLeftOfOperator * displayValue;
+    } else if (operatorInUse == '−') {
+        newValue = valueLeftOfOperator - displayValue;
+    } else if (operatorInUse == '+') {
+        newValue = valueLeftOfOperator + displayValue;
+    }
+
+    return newValue;
+}
+
+
+// ------------ Use Number & Decimal Buttons ------------ //
+function addDisplayText(value) {
+
+    // If the last operation was '=' and numbers are selected then nothing is saved before continuing 
+    if (operatorInUse == '=') removeDisplayText('C'); 
+
+    if (! isNaN(value) || (value == '.' && decimalInUse == false)) {
+        if (displayTextIsNothing == true && value == '.') {
+            displayText = '0.'
+        } else if (displayTextIsNothing == true && value != '.') {
+            displayText = value;
+        } else {
+            displayText = displayText + value;
+        }
+
+        displayTextIsNothing = false;
+        if (value == '.') decimalInUse = true;
+
+        document.getElementById('display').innerText = displayText;
+    }
+}
+
+// ----------- Use Clear & Backspace Buttons ----------- //
+function removeDisplayText(value) {
+
+    if (displayTextIsNothing == false && value == '←') {
+        if (displayText.length == 1) {
+            displayText = '0';
+            displayTextIsNothing = true;
+        } else if (displayText == '0.') {
+            displayText = '0';
+            displayTextIsNothing = true;
+            decimalInUse = false;
+        } else if (displayText.length > 1) {
+            if (displayText.substring(displayText.length - 1) == '.') decimalInUse = false;
+            displayText = displayText.substring(0, displayText.length-1)
+        }
+    } else if (value == 'CE' && displayTextIsNothing == false) {
+        displayText = '0';
+        displayTextIsNothing = true;
+        decimalInUse = false;
+    } else if (value == 'C') {
+        displayText = '0';
+        displayTextIsNothing = true;
+        decimalInUse = false;
+
+        displayTapeText = ''
+        operatorInUse = '';
+        valueLeftOfOperator = 0;
+    }
+
+    document.getElementById('display-tape').innerText = displayTapeText;
+    if (displayTapeText == '') document.getElementById('display-tape').innerHTML = '<br>';
+    document.getElementById('display').innerText = displayText;
+}
+
+
+// ----------------- Button Click Event ----------------- //
 document.body.addEventListener('click', function (evt) {
     if (evt.target.className == 'button btn-number') {
-        addScreenText(evt.target.innerText);
+        addDisplayText(evt.target.innerText);
     } else if (evt.target.className == 'button btn-clear') {
-        removeScreenText(evt.target.innerText);
+        removeDisplayText(evt.target.innerText);
     } else if (evt.target.className == 'button btn-operation') {
-        console.log(evt)
-    } else if (evt.target.className == 'button btn-memory') {
+        useOperator(evt.target.innerText);
+    }else if (evt.target.className == 'button btn-memory') {
         console.log(evt)
     }
     
 }, false);
 
-// ---------------------- Key Press ---------------------- //
+// ------------------- Key Press Event ------------------- //
 document.body.addEventListener('keydown', function (evt) {
     let keyId = evt.keyCode;
     switch(keyId) {
         case 8: // Backspace
-            removeScreenText('←');
+            removeDisplayText('←');
             break;
-        case 27: // Esc
-        removeScreenText('C');
-        break;
-        case 46: // Delete
-            removeScreenText('CE');
+        case 13: // Enter button
+            useOperator('=');
+            break;
+        case 27: // Esc button
+            removeDisplayText('C');
+            break;
+        case 46: // Delete button
+            removeDisplayText('CE');
+            break;
+        case 56: // 8 or * button
+            if (evt.key == '8') {
+                addDisplayText('8');
+            } else if (evt.key == '*') {
+                useOperator('×');
+            }
+            break;
+        case 106: // * on keypad
+            useOperator('×');
+            break;
+        case 107: // + on keypad
+            useOperator('+');
+            break;
+        case 109: // - on keypad
+            useOperator('−');
+            break;
+        case 111: // / on keypad
+            useOperator('÷');
+            break;
+        case 187: // = or + button 
+            useOperator(evt.key)
+            break;
+        case 189: // - or _ button
+            if (evt.key == '-') useOperator('−');
+            break;
+        case 191: // / or ? button
+            if (evt.key == '/') useOperator('/');
             break;
         default:
-            addScreenText(evt.key);
+            addDisplayText(evt.key);
     } // END SWITCH
 }, false);
-
-
-// ----------------- Calculator Display ----------------- //
-function addScreenText(value) {
-    if (! isNaN(value) || (value == '.' && decimalInUse === false)) {
-        if (screenTextIsNothing === true && value == '.') {
-            screenText = '0.'
-        } else if (screenTextIsNothing === true && value != '.') {
-            screenText = value;
-        } else {
-            screenText = screenText + value;
-        }
-
-        screenTextIsNothing = false;
-        if (value == '.') decimalInUse = true;
-
-        document.getElementById('result').innerText = screenText;
-    }
-}
-
-function removeScreenText(value) {
-    if (value == '←' && screenText == '0.') {
-        screenText = '0';
-        screenTextIsNothing = true;
-        decimalInUse = false;
-    } else if (value == '←' && screenText.length == 1) {
-        screenText = '0';
-        screenTextIsNothing = true;
-    } else if (value == '←' && screenText.length > 1) {
-        if (screenText.substring(0, screenText.length-1) == '.') decimalInUse = false;
-        screenText = screenText.substring(0, screenText.length-1)
-    } else if (value == 'CE') {
-        screenText = '0';
-        screenTextIsNothing = true;
-        decimalInUse = false;
-    } else if (value == 'C') {
-        screenText = '0';
-        screenTextIsNothing = true;
-        decimalInUse = false;
-        firstValue = 0;
-    }
-
-    document.getElementById('result').innerText = screenText;
-}
-
 
